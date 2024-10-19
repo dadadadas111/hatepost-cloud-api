1269b4f27ac41e9415229235f0307d34c6d357ae
dadadadas111 <long6athcskl@gmail.com>
2024-10-19 21:56:18 +0700

    feat: mongodb connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { LoggerMiddleware } from 'src/logger/logger.middleware';
 import { FirebaseMiddleware } from 'src/firebase/firebase.middleware';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     FirebaseModule,
     ConfigModule.forRoot(),
     AuthModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
   controllers: [AppController],
   providers: [AppService],
@@ -21,6 +23,6 @@ export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware, FirebaseMiddleware)
-      .forRoutes('/');
+      .forRoutes('/')
   }
 }

a9cd75af0d95c54555c5964c92c1797fd67166ba
dadadadas111 <long6athcskl@gmail.com>
2024-10-19 20:46:24 +0700

    feat: logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirebaseModule } from './firebase/firebase.module';
+import { LoggerMiddleware } from 'src/logger/logger.middleware';
+import { FirebaseMiddleware } from 'src/firebase/firebase.middleware';
 
 @Module({
-  imports: [],
+  imports: [FirebaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  // use logger middleware and firebase middleware for all routes
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware, FirebaseMiddleware)
+      .forRoutes('*');
+  }
+}
---
+++ src/logger/logger.middleware.ts
@@ -0,0 +1,18 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  async use(req: any, res: any, next: () => void) {
+    Logger.log({
+      request: {
+        method: req.method,
+        url: req.url,
+        headers: req.headers,
+        body: req.body,
+        params: req.params,
+        query: req.query,
+      },
+    }, 'HTTP Request');
+    next();
+  }
+}

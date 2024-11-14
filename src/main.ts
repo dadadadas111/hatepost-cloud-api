2e7d6791038f0240bb4f21c8ee5b4d3caae40ebe
dadadadas111 <long6athcskl@gmail.com>
2024-11-14 12:19:59 +0700

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://dadadadas111.github.io',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);

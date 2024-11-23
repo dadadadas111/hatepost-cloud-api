539752a76a5674636263fbb7f0d1d4d62fdcfc95
dadadadas111 <long6athcskl@gmail.com>
2024-11-23 09:51:40 +0700

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://fumi.fyi/',
+    origin: 'https://fumi.fyi',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });

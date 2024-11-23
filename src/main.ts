5bda1e72843bcc750b04648c4f1036f633afc799
dadadadas111 <long6athcskl@gmail.com>
2024-11-23 07:54:25 +0700

    smol migrating

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://dadadadas111.github.io',
+    origin: 'https://fumi.fyi/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
